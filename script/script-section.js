b867d65bfeb3a7c9d09bfa947a95feaaea57f295
Colin Jp <colinandgraffemotors@gmail.com>
2023-09-06 16:09:57 -0400

    Recien agregado

---
+++ script/script-section.js
@@ -0,0 +1,31 @@
+
+
+let selectOfDarkMode = document.getElementById("select-2");
+let LinkStyleSheet = document.querySelector("link")
+
+function  DarkModeSelected(){
+    switch(selectOfDarkMode.value){
+         case "Dark Mode":
+             LinkStyleSheet.setAttribute("href","/style/style.css")
+             break;
+        
+        case "Light Mode": 
+            LinkStyleSheet.setAttribute("href", "/style/LigthMode/style.css");
+            break;
+        
+        default:
+            LinkStyleSheet.setAttribute("href","/style/style.css")
+            break;
+
+    }
+}
+
+selectOfDarkMode.onchange = ()=>{
+    DarkModeSelected();
+}
+
+
+
+
+
+
