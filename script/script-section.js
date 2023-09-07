ef5b68f7a1da131517b4115068c7913153f90163
Colin Jp <colinandgraffemotors@gmail.com>
2023-09-07 14:12:42 -0400

    no funciona el buscador :(

--- script/script-section.js
+++ script/script-section.js
@@ -22,19 +22,3 @@ selectOfDarkMode.onchange = ()=>{
     DarkModeSelected();
 }
 
-
-let inputSearch = document.querySelector(".search-1");
-let search = ["Dark Lane Tapes Demo"]
-let searchSplit = search[0].split(" ");
-
-function searching (){
-    if(inputSearch.value === search[0] || inputSearch.value === searchSplit[0]){
-        
-    }else{
-        console.error("ERROR 404 NOT FUND")
-    }
-}
-
-inputSearch.addEventListener("keypress", searching)
-
-
