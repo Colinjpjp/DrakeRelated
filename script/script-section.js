6afb4d9e3d11710d5de8216df2e6c484834b95b7
Colin Jp <colinandgraffemotors@gmail.com>
2023-09-16 14:12:59 -0400

    no puedo terminrar el buscador falta poco

--- script/script-section.js
+++ script/script-section.js
@@ -1,41 +1,56 @@
-let selectOfDarkMode = document.getElementById("select-2");
-let LinkStyleSheet = document.querySelector("link")
+const selectOfDarkMode = document.getElementById("select-2");
+const LinkStyleSheet = document.querySelector("link");
 
-function  DarkModeSelected(){
-    switch(selectOfDarkMode.value){
-         case "Dark Mode":
-             LinkStyleSheet.setAttribute("href","./style/style.css")
-             break;
-        
-        case "Light Mode": 
-            LinkStyleSheet.setAttribute("href", "./style/LigthMode/style.css");
-            break;
-        
-        default:
-            LinkStyleSheet.setAttribute("href","./style/style.css")
-            break;
+function DarkModeSelected() {
+  switch (selectOfDarkMode.value) {
+    case "Dark Mode":
+      LinkStyleSheet.setAttribute("href", "./style/style.css");
+      break;
 
-    }
+    case "Light Mode":
+      LinkStyleSheet.setAttribute("href", "./style/LigthMode/style.css");
+      break;
+
+    default:
+      LinkStyleSheet.setAttribute("href", "./style/style.css");
+      break;
+  }
 }
 
-selectOfDarkMode.onchange = ()=>{
-    DarkModeSelected();
+const selectOfOrder = document.getElementById("select-1");
+const ProductsSection = document.querySelector(".Products");
+
+function change() {
+  const value = selectOfOrder.value;
+
+  if (value === "Mas recientes") {
+    return $(".Products").load("mode/MasRecientes.html");
+  } else if (value === "Mas vendidos") {
+    return $(".Products").load("mode/MasVendidos.html");
+  } else if (value === "Aclamados por la Critica") {
+    return $(".Products").load("/mode/AclamadosPorLaCritica.html");
+  } else if (value === "Precio Mas bajo") {
+    return $(".Products").load("mode/PrecioMasBajo.html");
+  } else if (value === "Precio Mas alto") {
+    return $(".Products").load("mode/PrecioMasAlto.html");
+  } else if (value === "Mas populares") {
+    return $(".Products").load("mode/MasPopulares.html");
+  }
 }
+selectOfOrder.onchange = change;
 
+$(function () {
+  $(".Products").load("mode/MasRecientes.html");
+});
 
-let names = document.querySelectorAll(".name-product");
-let inputSearch = document.querySelector(".search-1");
+let contenedor = document.querySelectorAll(".products");
 
-// function searching() {
-    document.addEventListener("keypress", e =>{
-        if(e.target.matches(".search-1")) {
-            document.querySelectorAll(".name-product").forEach(album =>{
-                album.textContent.toLowerCase().includes(e.target.value)
-                ? album.classList.remove("filtro")
-                : album.classList.add("filtro")
-            })
-        }
-    })
-// }
-
-// inputSearch.addEventListener("keypress", searching)
+addEventListener("keyup", (e) => {
+  if (e.target.matches(".search-1")) {
+    document.querySelectorAll(".name-product").forEach((names) => {
+      names.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+        ? names.classList.remove("filter")
+        : names.classList.add("filter");
+    });
+  }
+});
